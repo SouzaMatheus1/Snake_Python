b5b5f6138c93cc33e7475254aa7ae5a659b4252e
Matheus Souza <matheus3864@gmail.com>
2022-12-16 12:12:29 -0300

    Include ini.sh

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Snake_Python
- Snake game
+# The snake game made in python
+### First install the required libraries by terminal
+``` 
+$ pip install random
+$ pip install pygame
+```
+
+# O jogo da cobrinha feito em python
+### Primeiro instale as bibliotecas necessárias pelo terminal
+``` 
+$ pip install random
+$ pip install pygame
+```
---
+++ ini.sh
@@ -0,0 +1,36 @@
+BIBLIOTECA_RANDOM=$(pip freeze | grep random)
+BIBLIOTECA_PYGAME=$(pip freeze | grep pygame)
+APT_PIP=$(sudo apt list | grep python3-pip)
+
+clear
+if ['' = '$APT_PIP']; then
+    sudo apt install python3-pip
+
+    echo "\n--------------------------------"
+    echo "pip successfully installed! "
+else 
+    echo "pip already installed!"
+fi
+
+if ['' = '$BIBLIOTECA_RANDOM']; then
+    pip install random
+
+    echo "\n--------------------------------"
+    echo "Random library successfully installed! "
+else 
+    echo "Random library already installed!"
+fi
+
+if ['' = '$BIBLIOTECA_PYGAME']; then
+    pip install pygame
+
+    echo "\n--------------------------------"
+    echo "Pygame library successfully installed! "
+else 
+    echo "Pygame library already installed!"
+fi
+
+ echo "\n--------------------------------"
+ echo "Starting game..."
+
+python3 app.py
